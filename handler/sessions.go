04250ce3d10ce8fa52af93ad7fe8591bfadc78ec
seineo <1013248644@qq.com>
2023-02-26 00:56:52 +0800

    add register and logout handlers

--- handler/sessions.go
+++ handler/sessions.go
@@ -1,1 +1,24 @@
 package handler
+
+import (
+	"CloudDrive/middleware"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterSessionsRoutes(router *gin.Engine) {
+	group := router.Group("/api/v1/sessions")
+	group.POST("", login)
+	group.DELETE("current_session", middleware.AuthCheck, logout)
+}
+
+func login(c *gin.Context) {
+
+}
+
+func logout(c *gin.Context) {
+	session := sessions.Default(c)
+	session.Clear()
+	session.Save()
+	c.JSON(200, gin.H{})
+}
--- middleware/auth.go
+++ middleware/auth.go
@@ -4,16 +4,15 @@ import (
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
-	"net/http"
 )
 
 func AuthCheck(c *gin.Context) {
 	session := sessions.Default(c)
-	user := session.Get("id")
+	user := session.Get("user_id")
 	if user == nil {
-		log.Info("User has not logged in, Redirecting to login page...")
-		c.Redirect(http.StatusMovedPermanently, "/")
+		msg := "User has not logged in, authentication failed"
+		log.Info(msg)
+		c.JSON(401, gin.H{"error": msg})
 		c.Abort()
-		return
 	}
 }
