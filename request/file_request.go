6958fd83efd8da3ae5a7130fdd7a2ceaf1fb3e1e
seineo <1013248644@qq.com>
2023-07-05 16:46:11 +0800

    refactor: fix chunk handlers

--- request/file_request.go
+++ request/file_request.go
@@ -13,3 +13,16 @@ type FileRequest struct {
 	DirHash  string `json:"dirHash" binding:"required"`
 	FileSize uint   `json:"fileSize" binding:"required"`
 }
+
+type ChunkRequest struct {
+	FileHash    string `json:"fileHash" binding:"required"`
+	ChunkHash   string `json:"chunkHash" binding:"required"`
+	Index       uint   `json:"index" binding:"required"` // start from 1 to avoid binding required error when index is 0
+	TotalChunks uint   `json:"totalChunks" binding:"required"`
+	Blob        string `json:"blob" binding:"required"`
+}
+
+type CurrentChunks struct {
+	TotalChunks uint            `json:"totalChunks"`
+	Indexes     map[uint]string `json:"indexes"`
+}
